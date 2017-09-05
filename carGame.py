a851997430321fac47c8f4286d8663dc5758a4eb
CosmicCoder96 <abhinav.khare31@gmail.com>
2017-09-05 10:35:38 +0530

    MAJOR REFACTOR: Game made into a class

--- carGame.py
+++ carGame.py
@@ -1,15 +1,155 @@
 import pygame
+from PodSixNet.Connection import ConnectionListener, connection
 import sys
 import time
 from car import Car
 from obstacle import Obstacle
 
-checkErrors = pygame.init()
-if checkErrors[1] > 0:
-    print("(!) Had {0} initializign errors...exiting".format(checkErrors[1]))
-    sys.exit(-1)
-else:
-    print("(+) Game initialized successfully")
+class carGame(ConnectionListener):
+    def __init__(self):
+        self.checkErrors = pygame.init()
+        if self.checkErrors[1] > 0:
+            print("(!) Had {0} initializign errors...exiting".format(self.checkErrors[1]))
+            sys.exit(-1)
+        else:
+            print("(+) Game initialized successfully")
+        self.background = pygame.image.load("bg1.png")
+        # Screen
+        size = (800, 600)
+        self.screen = pygame.display.set_mode(size)
+        pygame.display.set_caption("Basic Scenery")
+        # List for all the sprites in the game
+
+        self.all_sprites_list = pygame.sprite.Group()
+        self.all_blocks_list = pygame.sprite.Group()
+
+        # The player sprite
+
+        self.playerCar = Car("car.png", (100, 100))
+        self.all_sprites_list.add(self.playerCar)
+
+        self.obstacles = {}
+        # The obstacle sprite
+        self.obstacles[0] = Obstacle("obstacleType1.png", (300, 200))
+        self.all_sprites_list.add(self.obstacles[0])
+        self.all_blocks_list.add(self.obstacles[0])
+
+        for i in range(1, 6):
+            self.obstacles[i] = Obstacle("obstacleType1.png", self.obstacles[i - 1].rect.topright)
+            self.all_sprites_list.add(self.obstacles[i])
+            self.all_blocks_list.add(self.obstacles[i])
+        self.obstacles[10] = Obstacle("obstacleType1.png", self.obstacles[5].rect.bottomleft)
+        self.all_sprites_list.add(self.obstacles[10])
+        self.all_blocks_list.add(self.obstacles[10])
+        for i in range(11, 14):
+            self.obstacles[i] = Obstacle("obstacleType1.png", self.obstacles[i - 1].rect.bottomleft)
+            self.all_sprites_list.add(self.obstacles[i])
+            self.all_blocks_list.add(self.obstacles[i])
+
+        self.obstacles[16] = Obstacle("obstacleType1.png", (0, 0))
+        self.all_sprites_list.add(self.obstacles[16])
+        self.all_blocks_list.add(self.obstacles[16])
+        for i in range(17, 36):
+            self.obstacles[i] = Obstacle("obstacleType1.png", self.obstacles[i - 1].rect.topright)
+            self.all_sprites_list.add(self.obstacles[i])
+            self.all_blocks_list.add(self.obstacles[i])
+
+        self.obstacles[37] = Obstacle("obstacleType1.png", self.obstacles[16].rect.bottomleft)
+        self.all_sprites_list.add(self.obstacles[37])
+        self.all_blocks_list.add(self.obstacles[37])
+        for i in range(38, 51):
+            self.obstacles[i] = Obstacle("obstacleType1.png", self.obstacles[i - 1].rect.bottomleft)
+            self.all_sprites_list.add(self.obstacles[i])
+            self.all_blocks_list.add(self.obstacles[i])
+
+        self.obstacles[51] = Obstacle("obstacleType1.png", self.obstacles[50].rect.topright)
+        self.all_sprites_list.add(self.obstacles[51])
+        self.all_blocks_list.add(self.obstacles[51])
+        for i in range(52, 70):
+            self.obstacles[i] = Obstacle("obstacleType1.png", self.obstacles[i - 1].rect.topright)
+            self.all_sprites_list.add(self.obstacles[i])
+            self.all_blocks_list.add(self.obstacles[i])
+
+        self.obstacles[70] = Obstacle("obstacleType1.png", self.obstacles[35].rect.bottomleft)
+        self.all_sprites_list.add(self.obstacles[70])
+        self.all_blocks_list.add(self.obstacles[70])
+        for i in range(71, 83):
+            self.obstacles[i] = Obstacle("obstacleType1.png", self.obstacles[i - 1].rect.bottomleft)
+            self.all_sprites_list.add(self.obstacles[i])
+            self.all_blocks_list.add(self.obstacles[i])
+        self.obstacles[83] = Obstacle("obstacleType1.png", (600, 100))
+        self.all_sprites_list.add(self.obstacles[83])
+        self.all_blocks_list.add(self.obstacles[83])
+        self.obstacles[84] = Obstacle("obstacleType1.png", (40, 400))
+        self.all_sprites_list.add(self.obstacles[84])
+        self.all_blocks_list.add(self.obstacles[84])
+        for i in range(85, 88):
+            self.obstacles[i] = Obstacle("obstacleType1.png", self.obstacles[i - 1].rect.bottomleft)
+            self.all_sprites_list.add(self.obstacles[i])
+            self.all_blocks_list.add(self.obstacles[i])
+        self.obstacles[88] = Obstacle("obstacleType1.png", (80, 440))
+        self.all_sprites_list.add(self.obstacles[88])
+        self.all_blocks_list.add(self.obstacles[88])
+        for i in range(89, 91):
+            self.obstacles[i] = Obstacle("obstacleType1.png", self.obstacles[i - 1].rect.bottomleft)
+            self.all_sprites_list.add(self.obstacles[i])
+            self.all_blocks_list.add(self.obstacles[i])
+        self.obstacles[91] = Obstacle("obstacleType1.png", (120, 480))
+        self.all_sprites_list.add(self.obstacles[91])
+        self.all_blocks_list.add(self.obstacles[91])
+        for i in range(92, 93):
+            self.obstacles[i] = Obstacle("obstacleType1.png", self.obstacles[i - 1].rect.bottomleft)
+            self.all_sprites_list.add(self.obstacles[i])
+            self.all_blocks_list.add(self.obstacles[i])
+        self.obstacles[93] = Obstacle("obstacleType1.png", (160, 520))
+        self.all_sprites_list.add(self.obstacles[93])
+        self.all_blocks_list.add(self.obstacles[93])
+        self.carryOn = True  # Game state variable
+
+        self.clock = pygame.time.Clock()
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.carryOn = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_x:
+                    self.carryOn = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.playerCar.rotateLeft(5)
+
+        if keys[pygame.K_RIGHT]:
+            self.playerCar.rotateRight(5)
+
+        if keys[pygame.K_UP]:
+            self.playerCar.moveForward(10)
+
+        if keys[pygame.K_DOWN]:
+            self.playerCar.moveBackward(10)
+        collision_list = pygame.sprite.spritecollide(self.playerCar, self.all_blocks_list, False)
+        self.all_sprites_list.update()
+        # Drawing on Screen
+        # screen.fill(GREEN)
+        self.screen.blit(self.background, (0, 0))
+
+        # Draw The Road
+        # pygame.draw.rect(screen, BLACK, [40, 0, 200, 300])
+        # Draw Line painting on the road
+        # pygame.draw.line(screen, WHITE, [140, 0], [140, 300], 5)
+
+        # Now let's draw all the sprites in one go. (For now we only have 1 sprite!)
+        self.all_sprites_list.draw(self.screen)
+
+        # Refresh Screen
+        pygame.display.flip()
+        self.clock.tick(90)
+        for car in collision_list:
+            print("Car crash!")
+            pygame.event.wait()
+            time.sleep(2)
+            # End Of Game
+            self.carryOn = False
+        
 
 # Define some colors
 BLACK = (0, 0, 0)
@@ -18,142 +158,9 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BROWN = (100,100,100)
 
-background = pygame.image.load("bg1.png")
-# Screen
-size = (800,600)
-screen = pygame.display.set_mode(size)
-pygame.display.set_caption("Basic Scenery")
-# List for all the sprites in the game
+game = carGame()
 
-all_sprites_list = pygame.sprite.Group()
-all_blocks_list = pygame.sprite.Group()
-
-
-# The player sprite
-
-playerCar = Car("car.png", (100,100))
-all_sprites_list.add(playerCar)
-
-obstacles = {}
-# The obstacle sprite
-obstacles[0] = Obstacle("obstacleType1.png", (300, 200))
-all_sprites_list.add(obstacles[0])
-all_blocks_list.add(obstacles[0])
-
-for i in range(1, 6):
-    obstacles[i] = Obstacle("obstacleType1.png", obstacles[i-1].rect.topright)
-    all_sprites_list.add(obstacles[i])
-    all_blocks_list.add(obstacles[i])
-obstacles[10] = Obstacle("obstacleType1.png", obstacles[5].rect.bottomleft)
-all_sprites_list.add(obstacles[10])
-all_blocks_list.add(obstacles[10])
-for i in range(11, 14):
-    obstacles[i] = Obstacle("obstacleType1.png", obstacles[i-1].rect.bottomleft)
-    all_sprites_list.add(obstacles[i])
-    all_blocks_list.add(obstacles[i])
-
-obstacles[16] = Obstacle("obstacleType1.png", (0,0))
-all_sprites_list.add(obstacles[16])
-all_blocks_list.add(obstacles[16])
-for i in range(17,36):
-    obstacles[i] = Obstacle("obstacleType1.png", obstacles[i-1].rect.topright)
-    all_sprites_list.add(obstacles[i])
-    all_blocks_list.add(obstacles[i])
-
-obstacles[37] = Obstacle("obstacleType1.png", obstacles[16].rect.bottomleft)
-all_sprites_list.add(obstacles[37])
-all_blocks_list.add(obstacles[37])
-for i in range(38, 51):
-    obstacles[i] = Obstacle("obstacleType1.png", obstacles[i-1].rect.bottomleft)
-    all_sprites_list.add(obstacles[i])
-    all_blocks_list.add(obstacles[i])
-
-obstacles[51] = Obstacle("obstacleType1.png", obstacles[50].rect.topright)
-all_sprites_list.add(obstacles[51])
-all_blocks_list.add(obstacles[51])
-for i in range(52,70):
-    obstacles[i] = Obstacle("obstacleType1.png", obstacles[i-1].rect.topright)
-    all_sprites_list.add(obstacles[i])
-    all_blocks_list.add(obstacles[i])
-
-obstacles[70] = Obstacle("obstacleType1.png", obstacles[35].rect.bottomleft)
-all_sprites_list.add(obstacles[70])
-all_blocks_list.add(obstacles[70])
-for i in range(71, 83):
-    obstacles[i] = Obstacle("obstacleType1.png", obstacles[i-1].rect.bottomleft)
-    all_sprites_list.add(obstacles[i])
-    all_blocks_list.add(obstacles[i])
-obstacles[83] = Obstacle("obstacleType1.png", (600,100))
-all_sprites_list.add(obstacles[83])
-all_blocks_list.add(obstacles[83])
-obstacles[84] = Obstacle("obstacleType1.png", (40,400))
-all_sprites_list.add(obstacles[84])
-all_blocks_list.add(obstacles[84])
-for i in range(85, 88):
-    obstacles[i] = Obstacle("obstacleType1.png", obstacles[i-1].rect.bottomleft)
-    all_sprites_list.add(obstacles[i])
-    all_blocks_list.add(obstacles[i])
-obstacles[88] = Obstacle("obstacleType1.png", (80,440))
-all_sprites_list.add(obstacles[88])
-all_blocks_list.add(obstacles[88])
-for i in range(89, 91):
-    obstacles[i] = Obstacle("obstacleType1.png", obstacles[i-1].rect.bottomleft)
-    all_sprites_list.add(obstacles[i])
-    all_blocks_list.add(obstacles[i])
-obstacles[91] = Obstacle("obstacleType1.png", (120,480))
-all_sprites_list.add(obstacles[91])
-all_blocks_list.add(obstacles[91])
-for i in range(92, 93):
-    obstacles[i] = Obstacle("obstacleType1.png", obstacles[i-1].rect.bottomleft)
-    all_sprites_list.add(obstacles[i])
-    all_blocks_list.add(obstacles[i])
-obstacles[93] = Obstacle("obstacleType1.png", (160,520))
-all_sprites_list.add(obstacles[93])
-all_blocks_list.add(obstacles[93])
-carryOn = True # Game state variable
-
-clock  = pygame.time.Clock()
-
-while carryOn:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            carryOn = False
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_x:
-                carryOn = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        playerCar.rotateLeft(5)
-
-    if keys[pygame.K_RIGHT]:
-        playerCar.rotateRight(5)
-
-    if keys[pygame.K_UP]:
-        playerCar.moveForward(10)
-
-    if keys[pygame.K_DOWN]:
-        playerCar.moveBackward(10)
-    collision_list = pygame.sprite.spritecollide(playerCar, all_blocks_list, False)
-    all_sprites_list.update()
-    # Drawing on Screen
-    # screen.fill(GREEN)
-    screen.blit(background, (0, 0))
-
-    # Draw The Road
-    # pygame.draw.rect(screen, BLACK, [40, 0, 200, 300])
-    # Draw Line painting on the road
-    # pygame.draw.line(screen, WHITE, [140, 0], [140, 300], 5)
-
-    # Now let's draw all the sprites in one go. (For now we only have 1 sprite!)
-    all_sprites_list.draw(screen)
-
-    # Refresh Screen
-    pygame.display.flip()
-    for car in collision_list:
-        print("Car crash!")
-        pygame.event.wait()
-        time.sleep(2)
-        # End Of Game
-        carryOn = False
-    clock.tick(90)
+while game.carryOn:
+    game.update()
+    
 pygame.quit()
